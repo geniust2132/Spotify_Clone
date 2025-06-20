7f330ee48e71da9e5dea9857408a5f2e9cadf8aa
geniust2132 <mhaque2132@gmail.com>
2025-06-21 01:28:34 +0530

    here var,DT,Obj is present

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 console.log("code is running") // this will be printed in console when we open it through inspect element
 
 
-var a = prompt("enter you number")
-var isTrue = confirm("are you sure to leave this page")
+var a = prompt("enter you number") //this is the popup type prompt that show 
+var isTrue = confirm("are you sure to leave this page") //another popup
 
 if (isTrue) {
         console.log("blasting")
@@ -12,4 +12,5 @@ if (isTrue) {
 else{
         console.log("not blasting")
 }
-console.log("your number is " + a)
+
+console.log("your number is " + a) //shows in the console log 
