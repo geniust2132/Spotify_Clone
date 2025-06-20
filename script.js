a0cfc1496a7642dec5aac7b8455dc682027baf99
geniust2132 <mhaque2132@gmail.com>
2025-06-20 23:44:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//alert("this is the alert message")  //when reload then this message will be shown
+
+console.log("code is running") // this will be printed in console when we open it through inspect element
+
+
+var a = prompt("enter you number")
+var isTrue = confirm("are you sure to leave this page")
+
+if (isTrue) {
+        console.log("blasting")
+}
+else{
+        console.log("not blasting")
+}
+console.log("your number is " + a)
